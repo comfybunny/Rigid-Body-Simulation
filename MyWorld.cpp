c3cacd3104c476c67e316466a289713545224d93
comfybunny <yanxialin2001@yahoo.com>
2016-03-16 22:48:25 -0400

    Fixed!
    Working properly!

--- MyWorld.cpp
+++ MyWorld.cpp
@@ -22,7 +22,7 @@ MyWorld::MyWorld() {
     rb1->mPosition[1] = -0.5;
     
     rb1->mAngMomentum = Vector3d(0.0, 0.01, 0.0);
-    mRigidBodies.push_back(rb1);
+    
     
     RigidBody *rb2 = new RigidBody(dart::dynamics::Shape::ELLIPSOID, Vector3d(0.06, 0.06, 0.06));
     mCollisionDetector->addRigidBody(rb2, "ellipse"); // Put rb2 in collision detector
@@ -31,6 +31,7 @@ MyWorld::MyWorld() {
     rb2->mAngMomentum = Vector3d(0.01, 0.0, 0.0);
     rb2->mColor = Vector4d(0.2, 0.8, 0.2, 1.0); // Blue
     mRigidBodies.push_back(rb2);
+	mRigidBodies.push_back(rb1);
 }
 
 void MyWorld::initializePinata() {
@@ -69,8 +70,8 @@ void MyWorld::simulate() {
     // your first job is to fill in the integration of orientation and angular momentum.
     for (int i = 0; i < mRigidBodies.size(); i++) {
 
-		mRigidBodies[i]->mQuatOrient.normalize();
-		
+		Eigen::Matrix3d rotMatrix = mRigidBodies[i]->mQuatOrient.toRotationMatrix();
+		Eigen::Vector3d w = (rotMatrix*(mRigidBodies[i]->Ibody)*rotMatrix.transpose()).inverse() * mRigidBodies[i]->mAngMomentum;
 
 
         // derivative of position and linear momentum
@@ -78,12 +79,10 @@ void MyWorld::simulate() {
         Eigen::Vector3d dLinMom = mRigidBodies[i]->mMass * mGravity + mRigidBodies[i]->mAccumulatedForce;
         
 		// derivative of orientation
-		Eigen::Matrix3d rotMatrix = mRigidBodies[i]->mQuatOrient.toRotationMatrix();
-		Eigen::Vector3d w = (rotMatrix*(mRigidBodies[i]->Ibody)*rotMatrix.transpose()).inverse()*mRigidBodies[i]->mAngMomentum;
+		
 		Eigen::Quaterniond qDot = getQdot(w, (mRigidBodies[i]->mQuatOrient));
 
 		// derivative of angular momentum (torque)
-		// QUESTION : This doesn't get updated until we get to the collision handler?
 
         // update position and linear momentum
         mRigidBodies[i]->mPosition += dPos * mTimeStep;
@@ -91,9 +90,11 @@ void MyWorld::simulate() {
 
 		//update angular momentum and orientation
 		mRigidBodies[i]->mAngMomentum += mRigidBodies[i]->mAccumulatedTorque*mTimeStep;
-
 		mRigidBodies[i]->mQuatOrient.w() += qDot.w()*mTimeStep;
 		mRigidBodies[i]->mQuatOrient.vec() += qDot.vec()*mTimeStep;
+
+
+		mRigidBodies[i]->mQuatOrient.normalize();
 
 		mRigidBodies[i]->mOrientation = mRigidBodies[i]->mQuatOrient.toRotationMatrix();
     }
@@ -131,8 +132,9 @@ void MyWorld::collisionHandling() {
 
 	for (int collision = 0; collision < numContacts; collision++) {
 		
-		RigidContact& curr = mCollisionDetector->getContact(collision);
-		
+		RigidContact curr = mCollisionDetector->getContact(collision);
+		Eigen::Vector3d normal = curr.normal;
+
 		double mass_a = 0;
 		double mass_b = 0;
 		double a_term = 0;
@@ -141,14 +143,15 @@ void MyWorld::collisionHandling() {
 		Eigen::Vector3d pb_dot_minus = curr.pinataVelocity;
 
 		// this is the pinata
+
 		if (curr.rb1 != nullptr){
 			mass_a = 1.0/curr.rb1->mMass;
 			Eigen::Matrix3d rotMatrix_a = curr.rb1->mQuatOrient.toRotationMatrix();
 			Eigen::Matrix3d IaInv = ((rotMatrix_a*(curr.rb1->Ibody)*rotMatrix_a.transpose()).inverse());
 			Eigen::Vector3d w_a = IaInv*(curr.rb1->mAngMomentum);
 			Eigen::Vector3d r_a = curr.point - curr.rb1->mPosition;
-			pa_dot_minus = curr.rb1->mLinMomentum / curr.rb1->mMass + (w_a).cross(curr.point);
-			a_term = curr.normal.dot((IaInv*(r_a.cross(curr.normal))).cross(r_a));
+			pa_dot_minus = curr.rb1->mLinMomentum / curr.rb1->mMass + (w_a).cross(r_a) + mGravity*mTimeStep;
+			a_term = normal.dot((IaInv*(r_a.cross(normal))).cross(r_a));
 		}
 		if (curr.rb2 != nullptr) {
 			mass_b = 1.0/curr.rb2->mMass;
@@ -156,41 +159,41 @@ void MyWorld::collisionHandling() {
 			Eigen::Matrix3d IbInv = (rotMatrix_b*(curr.rb2->Ibody)*rotMatrix_b.transpose()).inverse();
 			Eigen::Vector3d w_b = IbInv*(curr.rb2->mAngMomentum);
 			Eigen::Vector3d r_b = curr.point - curr.rb2->mPosition;
-			pb_dot_minus = curr.rb2->mLinMomentum / curr.rb2->mMass + (w_b).cross(curr.point);
-			b_term = curr.normal.dot((IbInv*(r_b.cross(curr.normal))).cross(r_b));
+			pb_dot_minus = curr.rb2->mLinMomentum / curr.rb2->mMass + (w_b).cross(r_b) + mGravity*mTimeStep;
+			b_term = normal.dot((IbInv*(r_b.cross(normal))).cross(r_b));
 		}
 		
-		double v_r_minus = curr.normal.dot(pa_dot_minus - pb_dot_minus);
+		double v_r_minus = normal.dot(pa_dot_minus - pb_dot_minus);
 	//	cout << v_r_minus << endl;
 		if (v_r_minus > 0) {
 			continue;
 		}
+
+
 		double j = -(1.0 + epsilon)*v_r_minus / (mass_a + mass_b + a_term + b_term);
 
 		
 		if (curr.rb1 != nullptr) {
-			cout << "rb1 != null" << endl;
-			cout << j*curr.normal << endl;
-			curr.rb1->mAccumulatedForce += j*curr.normal / mTimeStep;
-			curr.rb1->mAccumulatedTorque += ((curr.point - curr.rb1->mPosition).cross(j*curr.normal))/ mTimeStep;
+			//cout << "rb1 != null" << endl;
+			//cout << j*curr.normal << endl;
+			curr.rb1->mAccumulatedForce += j*normal / mTimeStep;
+			curr.rb1->mAccumulatedTorque += ((curr.point - curr.rb1->mPosition).cross(j*normal))/ mTimeStep;
 		}
 		if (curr.rb2 != nullptr) {
-			cout << "rb2 != null" << endl;
-			cout << j*curr.normal << endl;
-			curr.rb2->mAccumulatedForce -= j*curr.normal /mTimeStep;
-			curr.rb2->mAccumulatedTorque -= ((curr.point - curr.rb2->mPosition).cross(j*curr.normal))/mTimeStep;
+			//cout << "rb2 != null" << endl;
+			//cout << j*curr.normal << endl;
+			curr.rb2->mAccumulatedForce -= j*normal /mTimeStep;
+			curr.rb2->mAccumulatedTorque += ((curr.point - curr.rb2->mPosition).cross(-j*normal))/mTimeStep;
 		}
 		
 	}
 }
 
-Eigen::Quaterniond MyWorld::getQdot(const Eigen::Vector3d& w1, const Eigen::Quaterniond &q){
-	// double w = -1.0 * q.vec().dot(w);
-	Eigen::Vector3d w = w1 / 2.0;
+Eigen::Quaterniond MyWorld::getQdot(const Eigen::Vector3d& w, const Eigen::Quaterniond &q){
 	Eigen:Vector3d v = q.w()*w + w.cross(q.vec());
 	Eigen::Quaterniond res;
 	res.setIdentity();
-	res.w() = -1.0 * q.vec().dot(w);
-	res.vec() = v;
+	res.w() = -1.0 * q.vec().dot(w)/2.0;
+	res.vec() = v/2.0;
 	return res;
 }
